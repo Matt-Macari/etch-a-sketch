42c1cc4bdb618b87922c299b80e2116be74c6a48
Matt <mmacari9@gmail.com pwd>
2024-05-09 09:15:58 -0700

    added mouseover to js, updated css and html

--- etch.js
+++ etch.js
@@ -3,14 +3,29 @@ function makeGrid(numGrid) {
     const container = document.getElementById("div-container");
 
     for (i = 0; i < numGrid * numGrid; i++) {
-        container.innerHTML += '<div id="box"></div>';
-        const box = document.getElementById('box');
-        box.addEventListener('mouseover', colorSquare);
+        let grid = document.createElement("div");
+        grid.classList.add("grid");
+        container.appendChild(grid);
+        
+
+        //container.innerHTML += '<div id="box"></div>';
+        //const grid = document.querySelector('div');
+        grid.addEventListener(
+            "mouseover",
+            (event) => {
+
+                event.target.style.backgroundColor = "orange";
+                setTimeout(() => {
+                    event.target.style.backgroundColor = "";
+                }, 750);
+            },
+            false,
+        );
 
     };
 };
 
-makeGrid(24);
+makeGrid(16);
 
 
 
